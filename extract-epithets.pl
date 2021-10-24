c9e624457c5680884c3cc0afe6412fa00afdda5d
Tim Nordenfur <tim@gurka.se>
2021-10-25 00:19:25 +0200

    unpack

--- extract-epithets.pl
+++ extract-epithets.pl
@@ -15,21 +15,10 @@ my $EPITHETS_OFFSET = 0x13533b4+12 - $IMAGEBASE_OFFSET;
 my $EPITHET_LENGTH = 0x20;
 
 my $blob = read_binary( $FILENAME_EXE_X64 );
-say length $blob;
 
 for my $epithet_i ( 0..10 ) {
 #for my $epithet_i ( 0..747 ) {
-	printf "%3d: ", $epithet_i;
-	my $ep = substr( $blob, $EPITHETS_OFFSET + $epithet_i * $EPITHET_LENGTH, $EPITHET_LENGTH );
-	say str_to_hexstr($ep);
-}
-
-
-
-sub str_to_hexstr {
-	my $str = shift;
-	my $hexstr = join '', map { sprintf "%02x", ord } split //, $str;
-	my @chunks;
-	push @chunks, substr( $hexstr, 0, 4, "" ) while length($hexstr);
-	return join( ' ', @chunks );
+	my $epithet_bytes = substr( $blob, $EPITHETS_OFFSET + $epithet_i * $EPITHET_LENGTH, $EPITHET_LENGTH );
+	my ( $name_offset, @rest ) = unpack 'L'.('S'x14), $epithet_bytes;
+	printf "%3d: 0x%04x  %s\n", $epithet_i, $name_offset, "@rest";
 }
