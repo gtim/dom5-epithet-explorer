60c2691e333ecca6dd20438280abdf211e92f394
Tim Nordenfur <tim@gurka.se>
2021-10-25 00:42:23 +0200

    find address as quad instead of long

--- extract-epithets.pl
+++ extract-epithets.pl
@@ -16,11 +16,11 @@ my $EPITHET_LENGTH = 0x20;
 
 my $blob = read_binary( $FILENAME_EXE_X64 );
 
-for my $epithet_i ( 0..10 ) {
+for my $epithet_i ( 0..10, 323 ) {
 #for my $epithet_i ( 0..745 ) {
 	my $epithet_bytes = substr( $blob, $EPITHETS_OFFSET + $epithet_i * $EPITHET_LENGTH, $EPITHET_LENGTH );
-	my ( $name_offset, @rest ) = unpack 'L'.('S'x14), $epithet_bytes;
-	my $name = get_cstring( $blob, $name_offset - $IMAGEBASE_OFFSET - 0x40000000 );
+	my ( $name_offset, @rest ) = unpack 'Q'.('S'x12), $epithet_bytes;
+	my $name = get_cstring( $blob, $name_offset - $IMAGEBASE_OFFSET - 0x140000000 );
 
 	printf "%3d: %s (0x%x)\n", $epithet_i, $name, $name_offset;
 	printf "     @rest\n\n";
