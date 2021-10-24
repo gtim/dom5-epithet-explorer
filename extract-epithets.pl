319673a28d644ffc1211d8b9c7e331c70c08253a
Tim Nordenfur <tim@gurka.se>
2021-10-24 23:58:36 +0200

    offset fix

--- extract-epithets.pl
+++ extract-epithets.pl
@@ -11,13 +11,14 @@ use File::Slurper qw/read_binary/;
 my $FILENAME_EXE_X64 = 'Dominions554-64.exe';
 
 my $IMAGEBASE_OFFSET = 0x2200;
-my $EPITHETS_OFFSET = 0x13533b4-6 - $IMAGEBASE_OFFSET; 
+my $EPITHETS_OFFSET = 0x13533b4+12 - $IMAGEBASE_OFFSET; 
 my $EPITHET_LENGTH = 0x20;
 
 my $blob = read_binary( $FILENAME_EXE_X64 );
 say length $blob;
 
-for my $epithet_i ( 0..747 ) {
+for my $epithet_i ( 0..10 ) {
+#for my $epithet_i ( 0..747 ) {
 	printf "%3d: ", $epithet_i;
 	my $ep = substr( $blob, $EPITHETS_OFFSET + $epithet_i * $EPITHET_LENGTH, $EPITHET_LENGTH );
 	say str_to_hexstr($ep);
