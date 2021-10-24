14cb74f6fe16697be9a6be986893922ca03e1365
Tim Nordenfur <tim@gurka.se>
2021-10-25 01:00:29 +0200

    dom str, magic paths, +scales

--- extract-epithets.pl
+++ extract-epithets.pl
@@ -16,14 +16,46 @@ my $EPITHET_LENGTH = 0x20;
 
 my $blob = read_binary( $FILENAME_EXE_X64 );
 
-for my $epithet_i ( 0..10, 323 ) {
+for my $epithet_i ( 0..20, 323 ) {
 #for my $epithet_i ( 0..745 ) {
 	my $epithet_bytes = substr( $blob, $EPITHETS_OFFSET + $epithet_i * $EPITHET_LENGTH, $EPITHET_LENGTH );
-	my ( $name_offset, @rest ) = unpack 'Q'.('S'x12), $epithet_bytes;
+	my ( $name_offset, @conditions_and_values ) = unpack 'Q'.('S'x12), $epithet_bytes;
 	my $name = get_cstring( $blob, $name_offset - $IMAGEBASE_OFFSET - 0x140000000 );
 
-	printf "%3d: %s (0x%x)\n", $epithet_i, $name, $name_offset;
-	printf "     @rest\n\n";
+	my @conditions = @conditions_and_values[0..5];
+	my @values = @conditions_and_values[6..11];
+
+	printf "%3d: %s\n", $epithet_i, $name;
+	for my $con_i ( 0..5 ) {
+		my ( $con, $val ) = ( $conditions[$con_i], $values[$con_i] );
+		my $constr;
+		given ( $con ) {
+
+			when (0) { next }
+
+			when (   1) { $constr = "Dom. Str.  >= $val"; }
+
+			when (1000) { $constr = "Fire       >= $val"; }
+			when (1001) { $constr = "Air        >= $val"; }
+			when (1002) { $constr = "Water      >= $val"; }
+			when (1003) { $constr = "Earth      >= $val"; }
+			when (1004) { $constr = "Astral     >= $val"; }
+			when (1005) { $constr = "Death      >= $val"; }
+			when (1006) { $constr = "Nature     >= $val"; }
+			when (1007) { $constr = "Blood      >= $val"; }
+
+			when (2100) { $constr = "Order      >= $val"; }
+			when (2101) { $constr = "Production >= $val"; }
+			when (2102) { $constr = "Heat       >= $val"; }
+			when (2103) { $constr = "Growth     >= $val"; }
+			when (2104) { $constr = "Luck       >= $val"; }
+			when (2105) { $constr = "Magic      >= $val"; }
+
+			default { $constr = sprintf "?? %d: %d", $con, $val; }
+		}
+		say '     '.$constr;
+	}
+	print "\n";
 }
 
 sub get_cstring {
