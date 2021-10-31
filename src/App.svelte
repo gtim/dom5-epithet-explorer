2ab524479fbb29e55f93031433c384e9683a92be
Tim Nordenfur <tim@gurka.se>
2021-10-31 23:57:03 +0100

    display selected epithets

--- src/App.svelte
+++ src/App.svelte
@@ -4,22 +4,36 @@
 	import * as eps from './epithets.json';
 
 	let selectedEpithet;
+	let selectedEpithets = [eps.epithets[0], eps.epithets[200]];
+
+	$: if ( selectedEpithet ) {
+		selectedEpithets.push( selectedEpithet );
+		selectedEpithets = selectedEpithets;
+	}
 
 </script>
 
 <main>
 	<AutoComplete 
-	  items={eps.epithets} bind:selectedItem={selectedEpithet}
-	  labelFieldName="string"
-	  valueFieldName="id"
+		items={eps.epithets} bind:selectedItem={selectedEpithet}
+		labelFieldName="string"
+		valueFieldName="id"
 	/>
+	<div style="display:flex;justify-content:center;">
+		{#each selectedEpithets as ep}
+			<div style="flex-direction:row;">{ep.string}</div>
+		{/each}
+		<div>
+		</div>
+	</div>
+
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
+		max-width: 480px;
 		margin: 0 auto;
 	}
 
