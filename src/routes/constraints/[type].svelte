86c1abc4649459c9ffdd0bcc07d6a22bc8a9ad20
Tim Nordenfur <tim@gurka.se>
2021-11-23 11:29:05 +0100

    display all epithets with N constraints

--- src/routes/constraints/[type].svelte
+++ src/routes/constraints/[type].svelte
@@ -1,14 +1,35 @@
 <script context="module">
 	export async function load({ page, fetch, session, stuff }) {
-		return { props: { ctype: page.params.type } };
+		if ( page.params.type === 'nature' ) {
+			return { props: { ctype: page.params.type } };
+		}
 	}
 </script>
 
 <script>
 	export let ctype;
+
+	import EpithetList from '$lib/EpithetList.svelte';
+	import { AllEpithets } from '$lib/AllEpithets.js';
+
+	// Filter epithets for N constraints
+
+	let filteredEpithets = $AllEpithets.filter( function (epithet) {
+		for ( const constraint of epithet.constraints ) {
+			if ( constraint.type === "magic path" && constraint.field === "N" ) {
+				epithet.sort = constraint.value;
+				return true;
+			}
+		}
+		return false;
+	} ).sort( (ep_a,ep_b) => ep_b.sort - ep_a.sort );
+
 </script>
 
 <main>
 	the ctype is { ctype }
+
+	<EpithetList epithets={filteredEpithets} />
+
 </main>
 
