f6bd048d2d9ed00226ffd45bdd1b266b5b61f569
Tim Nordenfur <tim@gurka.se>
2021-11-23 10:27:06 +0100

    routing for constraint-type page

---
+++ src/routes/constraints/[type].svelte
@@ -0,0 +1,14 @@
+<script context="module">
+	export async function load({ page, fetch, session, stuff }) {
+		return { props: { ctype: page.params.type } };
+	}
+</script>
+
+<script>
+	export let ctype;
+</script>
+
+<main>
+	the ctype is { ctype }
+</main>
+
