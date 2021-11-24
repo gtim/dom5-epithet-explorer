b6b1c871057600e50b90afc2fceb74ad19087fe6
Tim Nordenfur <tim@gurka.se>
2021-11-24 19:43:32 +0100

    preliminary lists page

---
+++ src/routes/lists.svelte
@@ -0,0 +1,38 @@
+<svelte:head>
+	<title>Epithet Lists - Dominions 5</title>
+</svelte:head>
+
+<p>
+	<a href="/fire">F</a>
+	<a href="/air">A</a>
+	<a href="/water">W</a>
+	<a href="/earth">E</a>
+	<a href="/astral">S</a>
+	<a href="/death">D</a>
+	<a href="/nature">N</a>
+	<a href="/blood">B</a>
+</p>
+<p>
+	<a href="/order">O</a>
+	<a href="/productivity">P</a>
+	<a href="/heat">H</a>
+	<a href="/growth">G</a>
+	<a href="/luck">L</a>
+	<a href="/magic">M</a>
+</p>
+<p>
+	<a href="/turmoil">T</a>
+	<a href="/sloth">S</a>
+	<a href="/cold">C</a>
+	<a href="/death-scale">D</a>
+	<a href="/misfortune">M</a>
+	<a href="/drain">D</a>
+</p>
+<p>
+	<a href="/male">male</a>
+	<a href="/female">female</a>
+	<a href="/undead">undead</a>
+	<a href="/demon">demon</a>
+	<a href="/immobile">immobile</a>
+	<a href="/inanimate">inanimate</a>
+</p>
