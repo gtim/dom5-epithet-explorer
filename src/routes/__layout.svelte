0ab14ed9c53ebb7354064388068cb2959126283f
Tim Nordenfur <tim@gurka.se>
2021-11-24 19:33:35 +0100

    navigation tabs

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,7 +6,10 @@
 	
 	<div class="content">
 
-		<h1>Epithet Explorer</h1>
+		<ul class="nav">
+			<li class="active"><a href="/">Epithet Search</a></li><!--
+			--><li><a href="/requirement">Requirements</a></li>
+		</ul>
 
 		<slot/>
 
@@ -33,6 +36,53 @@
 </footer>
 
 <style>
+	
+	/* Nav bar */
+
+	ul.nav {
+		list-style:none;
+		text-align:center;
+		padding:0;
+		margin:20px 0 40px 0;
+	}
+	ul.nav li {
+		display: inline-block;
+		border-bottom: none;
+		height: 50px;
+		line-height: 50px;
+		font-size: 1.4em;
+		margin:0;
+		padding:0;
+		/*
+		border-style:solid;
+		border-color:#fcfcfc;
+		border-width:0 0 1px 0;
+		*/
+	}
+	ul.nav li a {
+		text-decoration: none;
+		color: #7353BA;
+		display: block;
+		transition: .3s color;
+		margin:0;
+		padding:0 20px;
+	}
+	ul.nav a:hover {
+		color: #FAA6FF;
+	}
+	ul.nav li.active a {
+		color: #fcfcfc;
+		cursor: default;
+	}
+	ul.nav li.active {
+		/*
+		border-width:1px 1px 0 1px;
+		height:51px;
+		*/
+	}
+
+	/* main content */
+
 	main {
 		display:flex;
 		flex-direction:column;
@@ -41,23 +91,18 @@
 		margin: 0 auto;
 		text-align:center;
 	}
-	h1 {
-		text-align:center;
-		font-size:32px;
-		margin:16px 0 40px 0;
+	@media (max-width: 480px) {
+		main {
+			max-width: 480px;
+		}
 	}
+	
+	/* footer */
+
 	p.notes {
 		margin:8px auto 16px auto;
 		padding:0;
 		font-size:12px;
 		text-align:left;
-	}
-	@media (max-width: 480px) {
-		h1 {
-			margin:8px 0 16px 0;
-		}
-		main {
-			max-width: 480px;
-		}
 	}
 </style>
