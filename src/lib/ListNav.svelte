fbb3ef9bd85bf1a2e3be50dd043bb0f28d6971bb
Tim N <tim@gurka.se>
2021-12-15 23:19:07 +0100

    image links for paths and scales in nav

--- src/lib/ListNav.svelte
+++ src/lib/ListNav.svelte
@@ -1,31 +1,35 @@
 <script>
   import ListNavLink from '$lib/ListNavLink.svelte'
+  import ListNavImageLink from '$lib/ListNavImageLink.svelte'
 </script>
 
 <ul class="listnav">
-	<ListNavLink href="/fire/">F</ListNavLink>
-	<ListNavLink href="/air/">A</ListNavLink>
-	<ListNavLink href="/water/">W</ListNavLink>
-	<ListNavLink href="/earth/">E</ListNavLink>
-	<ListNavLink href="/astral/">S</ListNavLink>
-	<ListNavLink href="/death/">D</ListNavLink>
-	<ListNavLink href="/nature/">N</ListNavLink>
-	<ListNavLink href="/blood/">B</ListNavLink>
+	<ListNavImageLink href="/fire/"   src="/img/Path_F.png" title="Fire" />
+	<ListNavImageLink href="/air/"    src="/img/Path_A.png" title="Air"  />
+	<ListNavImageLink href="/water/"  src="/img/Path_W.png" title="Water" />
+	<ListNavImageLink href="/earth/"  src="/img/Path_E.png" title="Earth" />
+	<ListNavImageLink href="/astral/" src="/img/Path_S.png" title="Astral" />
+	<ListNavImageLink href="/death/"  src="/img/Path_D.png" title="Death" />
+	<ListNavImageLink href="/nature/" src="/img/Path_N.png" title="Nature" />
+	<ListNavImageLink href="/blood/"  src="/img/Path_B.png" title="Blood" />
 </ul>
 <br/>
 <ul class="listnav">
-	<ListNavLink href="/order/">O</ListNavLink>
-	<ListNavLink href="/productivity/">P</ListNavLink>
-	<ListNavLink href="/heat/">H</ListNavLink>
-	<ListNavLink href="/growth/">G</ListNavLink>
-	<ListNavLink href="/luck/">L</ListNavLink>
-	<ListNavLink href="/magic/">M</ListNavLink>
-	<ListNavLink href="/turmoil/">T</ListNavLink>
-	<ListNavLink href="/sloth/">S</ListNavLink>
-	<ListNavLink href="/cold/">C</ListNavLink>
-	<ListNavLink href="/death-scale/">D</ListNavLink>
-	<ListNavLink href="/misfortune/">M</ListNavLink>
-	<ListNavLink href="/drain/">D</ListNavLink>
+	<ListNavImageLink href="/order/" src="/img/Scale_order.png" title="Order" />
+	<ListNavImageLink href="/productivity/" src="/img/Scale_productivity.png" title="Productivity" />
+	<ListNavImageLink href="/heat/" src="/img/Scale_heat.png" title="Heat" />
+	<ListNavImageLink href="/growth/" src="/img/Scale_growth.png" title="Growth" />
+	<ListNavImageLink href="/luck/" src="/img/Scale_luck.png" title="Luck" />
+	<ListNavImageLink href="/magic/" src="/img/Scale_magic.png" title="Magic" />
+</ul>
+<br/>
+<ul class="listnav">
+	<ListNavImageLink href="/turmoil/" src="/img/Scale_turmoil.png" title="Turmoil" />
+	<ListNavImageLink href="/sloth/" src="/img/Scale_sloth.png" title="Sloth" />
+	<ListNavImageLink href="/cold/" src="/img/Scale_cold.png" title="Cold" />
+	<ListNavImageLink href="/death-scale/" src="/img/Scale_death.png" title="Death" />
+	<ListNavImageLink href="/misfortune/" src="/img/Scale_misfortune.png" title="Misfortune" />
+	<ListNavImageLink href="/drain/" src="/img/Scale_drain.png" title="Drain" />
 </ul>
 <br/>
 <ul class="listnav">
---
+++ src/lib/ListNavImageLink.svelte
@@ -0,0 +1,41 @@
+<script>
+	export let href, src, title;
+
+	import { page } from "$app/stores"
+	$: isActive = ( $page.path === href )
+
+</script>
+
+
+<li class:active={isActive}>
+	<a {href}><img {src} {title} alt="{title}"/></a>
+</li>
+
+
+<style>
+	li {
+		display: inline-block;
+		height: 48px;
+		margin:0;
+		padding:0;
+	}
+	li a {
+		text-decoration: none;
+		border:none;
+		display: block;
+		margin:0;
+		padding:0 6px;
+		min-width:34px;
+		height:48px;
+		border: solid 1px transparent;
+	}
+	a:hover {
+		border: solid 1px #FAA6FF;
+	}
+	li.active a {
+		color: #fcfcfc;
+	}
+	img {
+		vertical-align:middle;
+	}
+</style>
