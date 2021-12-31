00305bcad31b1c6573ea63909118209facbbf80e
Tim N <tim@gurka.se>
2021-12-31 13:12:15 +0100

    titles for all epithet list pages

--- src/routes/[type].svelte
+++ src/routes/[type].svelte
@@ -9,7 +9,6 @@
 			/* Magic Path */
 			let path_abbrev = page.params.type === 'astral' ? 'S' : page.params.type.charAt(0).toUpperCase();
 			return { props: {
-				ctype: page.params.type,
 				cfilter: (c) => c.type === "magic path" && c.field === path_abbrev,
 				header_image: '/img/Path_' + path_abbrev + '.png',
 				header_title: firstToUpperCase(page.params.type) + ' Pretender Epithets'
@@ -18,65 +17,60 @@
 			/* Scale */
 			var scale = page.params.type === 'death-scale' ? 'death' : page.params.type;
 			return { props: {
-				ctype: scale + '-scale',
 				cfilter: (c) => c.type === "scale" && c.field.toLowerCase() === scale,
 				header_image: '/img/Scale_' + scale + '.png',
 				header_title: firstToUpperCase(scale) + ' Pretender Epithets',
 				header_text: scale === 'order' ? 'The poetry and song Order-65535 bug is on my to-fix list.' : ''
 			} }; 
-		} else {
-			switch ( page.params.type ) {
-				case 'male':
-					return { props: { ctype: page.params.type, cfilter: (c) => c.type === "gender" && c.value === "male" } }; break;
-				case 'female':
-					return { props: { ctype: page.params.type, cfilter: (c) => c.type === "gender" && c.value === "female" } }; break;
-
-				case 'undead':
-				case 'demon':
-				case 'immortal': // none at the time of coding
-				case 'immobile':
-				case 'inanimate':
-				case 'disciple':
-					return { props: {
-						ctype: page.params.type,
-						cfilter: (c) => c.type === "boolean" && c.field === page.params.type && c.value === 1,
-					} };
-					break;
-
-				case 'chassis':
-					return { props: {
-						ctype: page.params.type,
-						cfilter: (c) => c.type === "chassis",
-						pretender_phrase: "These are the epithets unique to specific pretender chassis."
-					} }; break;
-				case 'nation':
-					return { props: {
-						ctype: page.params.type,
-						cfilter: (c) => c.type === "nation",
-						pretender_phrase: "These are the epithets unique to specific nations."
-					} }; break;
-
-				case 'special':
-					return { props: {
-						ctype: page.params.type,
-						cfilter: (c) => (
-							c.type === "misc minimum" && ( c.field === "eyes" || c.field === "fear" || c.field === "awe" || c.field === "strength" )
-							|| c.type === "default"
-						),
-						pretender_phrase: "These are the epithets requiring special properties not in the other lists."
-					} }; break;
-			}
+		} else if ( page.params.type === 'chassis' ) {
+			return { props: {
+				cfilter: (c) => c.type === "chassis",
+				header_image: '/img/pretender/2466.png',
+				header_title: 'Chassis-specific Epithets'
+			} }; 
+		} else if ( page.params.type === 'nation' ) {
+			return { props: {
+				cfilter: (c) => c.type === "nation",
+				header_title: 'Nation-specific Epithets'
+			} }; 
+		} else if ( page.params.type === 'male' ) {
+			return { props: { 
+				cfilter: (c) => c.type === "gender" && c.value === "male",
+				header_title: 'Male Pretender Epithets'
+			} };
+		} else if ( page.params.type === 'female' ) {
+			return { props: { 
+				cfilter: (c) => c.type === "gender" && c.value === "female",
+				header_title: 'Female Pretender Epithets'
+			} };
+		} else if ( page.params.type === 'disciple' ) {
+			return { props: { 
+				cfilter: (c) => c.type === "boolean" && c.field === 'disciple' && c.value === 1,
+				header_title: 'Disciple Epithets'
+			} };
+		} else if ( [ 'undead', 'demon', 'immortal', 'immobile', 'inanimate' ].includes( page.params.type ) ) {
+			// no immortal epithets at time of coding
+			return { props: {
+				cfilter: (c) => c.type === "boolean" && c.field === page.params.type && c.value === 1,
+				header_title: firstToUpperCase(page.params.type) + ' Pretender Epithets'
+			} };
+		} else if ( page.params.type === 'special' ) {
+			return { props: {
+				cfilter: (c) => (
+					c.type === "misc minimum" && ( c.field === "eyes" || c.field === "fear" || c.field === "awe" || c.field === "strength" )
+					|| c.type === "default"
+				),
+				header_title: "Special Epithets"
+			} };
 		}
 	}
 </script>
 
 <script>
-	export let ctype;
 	export let cfilter;
 	export let header_image;
 	export let header_title;
 	export let header_text;
-	export let pretender_phrase = "";
 
 	import EpithetList from '$lib/EpithetList.svelte';
 	import ListNav from '$lib/ListNav.svelte';
@@ -95,41 +89,27 @@
 		return false;
 	} ).sort( (ep_a,ep_b) => ep_b.sort - ep_a.sort );
 
-	let ctypeCapitalisedFirst;
-	$: ctypeCapitalisedFirst = ctype.charAt(0).toUpperCase() + ctype.slice(1);
-
 </script>
 
 	<svelte:head>
-		<title>{ctypeCapitalisedFirst} Pretender Epithets in Dominions 5</title>
+		<title>{header_title} in Dominions 5</title>
 	</svelte:head>
 
 
-	{#if header_title}
-		<h1>
+	<h1>
 		{#if header_image}
 			<img src="{header_image}" style="transform:scaleX(-1);"/>
 		{/if}
+
 		{header_title}
+
 		{#if header_image}
 			<img src="{header_image}"/>
 		{/if}
-		</h1>
-		{#if header_text}
-			<p>{header_text}</p>
-		{/if}
-	{:else}
-		<p>
-		{#if pretender_phrase}
-			{pretender_phrase}
-		{:else}
-			{#if filteredEpithets.length == 1}
-				This is the only epithet unique to {ctype} pretenders.
-			{:else}
-				These are the {filteredEpithets.length} epithets unique to {ctype} pretenders.
-			{/if}
-		{/if}
-		</p>
+	</h1>
+
+	{#if header_text}
+		<p>{header_text}</p>
 	{/if}
 
 	<EpithetList epithets={filteredEpithets} />
