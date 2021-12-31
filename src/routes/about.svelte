8b37869c9756dc3d58af4ac0fc1d7af296343421
Tim N <tim@gurka.se>
2021-12-31 02:00:56 +0100

    about page: add discord link, remove second paragraph

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -10,14 +10,17 @@
 	<p>
 		This is a tool for the game Dominions 5.
 		You can search for your opponents' epithets to figure out their pretender designs, or just browse the lists of Illwinter's imaginations.
-		Ideas, feedback and bug reports are very welcome on <a rel="external" href="https://github.com/gtim/dom5-epithet-explorer">github</a> and the dom5 discords (@timotej).
+		Feedback is welcome on <a rel="external" href="https://github.com/gtim/dom5-epithet-explorer">github</a> and the
+		<a rel="external" href="https://illwiki.com/dom5/dominions-discord-servers">dom5 discords</a> (@timotej).
 	</p>
+	<!--
 	<p>
 		Thank-you to <a rel="external" href="http://illwinter.com/dom5/">Illwinter</a> for creating my favorite game,
 		to Indract for doing the original binary-diving for Dominions 4,
 		to Larzm for the inspector,
 		and to you for checking out my little project!
 	</p>
+	-->
 	<p style="text-align:right;">
 		/Tim
 	</p>
